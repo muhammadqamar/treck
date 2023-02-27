a45cab1a0c545380070100bdc2a4b99e3feaefa9
muhammadqamar <muhammadqamar111@gmail.com>
2023-02-27 12:34:15 +0500

    first commit

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+// $('.recall-progress-bar').circleProgress({
+//   value: 0.35,
+//   size: 80.0,
+//   fill: {
+//     gradient: ['#CA2738'], // or color: '#3aeabb', or image: 'http://i.imgur.com/pT0i89v.png'
+//   },
+// });
+
+// $('.pa-progress-bar').circleProgress({
+//   value: 0.35,
+//   size: 80.0,
+//   fill: {
+//     gradient: ['#1FA5D4'], // or color: '#3aeabb', or image: 'http://i.imgur.com/pT0i89v.png'
+//   },
+// });
+
+// $('.pb-progress-bar').circleProgress({
+//   value: 0.35,
+//   size: 80.0,
+//   fill: {
+//     gradient: ['#1FA5D4'], // or color: '#3aeabb', or image: 'http://i.imgur.com/pT0i89v.png'
+//   },
+// });
+// $('.apply-progress-bar').circleProgress({
+//   value: 0.35,
+//   size: 80.0,
+//   fill: {
+//     gradient: ['#9FC33B'], // or color: '#3aeabb', or image: 'http://i.imgur.com/pT0i89v.png'
+//   },
+// });
